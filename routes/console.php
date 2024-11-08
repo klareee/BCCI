a4e7b41b184b6bc5dd07757f4ec6edc51ffab187
Miyunecadz <jvcadz@gmail.com>
2024-11-09 01:19:12 +0800

    update schedule

--- routes/console.php
+++ routes/console.php
@@ -7,13 +7,16 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+    (new MonthlyPayroll)->dispatchSync();
+})->lastDayOfMonth('20:00');
 
-
-Schedule::job(new MonthlyPayroll)->lastDayOfMonth('20:00');
-Schedule::job(new WeeklyPayroll)->weeklyOn(6, '20:00');
+Schedule::call(function () {
+    (new WeeklyPayroll)->dispatchSync();
+})->weeklyOn(6, '20:00');
 
 $endDayOfMonth = now()->endOfMonth()->format('m');
-Schedule::job(new BiMonthlyPayroll)->twiceMonthly(15, $endDayOfMonth,'20:00');
+Schedule::call(function () {
+    (new BiMonthlyPayroll)->dispatchSync();
+})
+    ->twiceMonthly(15, $endDayOfMonth, '20:00');
