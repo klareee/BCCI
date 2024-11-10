8fc7a3b9522ea93011592885b4728fee5026e5c0
Miyunecadz <jvcadz@gmail.com>
2024-11-11 01:25:44 +0800

    update UI

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -5,89 +5,50 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("User information cannot be updated, due to tracking of employees. If the information is wrong kindly contact your Admin or HR.") }}
         </p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+    {{-- <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
-    </form>
+    </form> --}}
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
+    <div class="mt-6 space-y-6">
 
         <div>
             <x-input-label for="first_name" :value="__('First Name')" />
-            <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" required autofocus autocomplete="name" />
+            <x-text-input id="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" readonly autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
         </div>
 
         <div>
             <x-input-label for="middle_name" :value="__('Middle Name')" />
-            <x-text-input id="middle_name" name="middle_name" type="text" class="mt-1 block w-full" :value="old('middle_name', $user->middle_name)"/>
-            <x-input-error class="mt-2" :messages="$errors->get('middle_name')" />
+            <x-text-input id="middle_name" type="text" class="mt-1 block w-full" :value="old('middle_name', $user->middle_name)" readonly/>
+            {{-- <x-input-error class="mt-2" :messages="$errors->get('middle_name')" /> --}}
         </div>
 
         <div>
             <x-input-label for="last_name" :value="__('Last Name')" />
-            <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" required/>
+            <x-text-input id="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" readonly/>
             <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
         </div>
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
+            <x-text-input id="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" readonly autocomplete="username" />
+            {{-- <x-input-error class="mt-2" :messages="$errors->get('email')" /> --}}
         </div>
 
         <div>
             <x-input-label for="date_of_birth" :value="__('Date of Birth')" />
-            <x-text-input id="date_of_birth" name="date_of_birth" type="date" class="mt-1 block w-full" :value="old('date_of_birth', $user->date_of_birth)" required/>
-            <x-input-error class="mt-2" :messages="$errors->get('date_of_birth')" />
+            <x-text-input id="date_of_birth" type="date" class="mt-1 block w-full" :value="old('date_of_birth', $user->date_of_birth)" readonly/>
+            {{-- <x-input-error class="mt-2" :messages="$errors->get('date_of_birth')" /> --}}
         </div>
 
         <div>
             <x-input-label for="gender" :value="__('Gender')" />
-            <select name="gender" id="gender">
-                <option value="">Select one</option>
-                @for (App\Enums\GenderEnum::cases() as $gender)
-                    <option value="{{$gender}}">{{ Str::title($gender) }}</option>
-                @endfor
-            </select>
-            <x-input-error class="mt-2" :messages="$errors->get('gender')" />
+            <x-text-input id="date_of_birth" type="text" class="mt-1 block w-full" value="{{ Str::title($user->gender) }}" readonly/>
+            {{-- <x-input-error class="mt-2" :messages="$errors->get('gender')" /> --}}
         </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
+    </div>
 </section>
